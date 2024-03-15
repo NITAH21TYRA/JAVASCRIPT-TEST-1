7b37d4bda2f9142414ca8f8b03af8aa0fa2a9d57
NANSALIRE_ANITAH <anitanansa@gmail.com>
2024-03-15 11:32:02 +0300

    JAVASCRIPT-TEST-1

---
+++ qtn_8.js
@@ -0,0 +1,11 @@
+const favoriteMovie = {
+    title: "Puff the Magic Dragon", 
+    duration: 30, 
+    star: {
+      name: "Puff", 
+      age: 45, 
+      country: "Uganda"
+    }
+  }
+  
+  console.log(favoriteMovie);
---
+++ qtn_9.js
@@ -0,0 +1,18 @@
+function fibonacci(n) {
+    if (n === 0) {
+      return 0;
+    } else if (n === 1) {
+      return 1;
+    } else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
+  
+  function printFibonacciUpTo(limit) {
+   
+    for (let i = 0; i < limit; i++) {
+      console.log(fibonacci(i));
+    }
+  }
+  
+  printFibonacciUpTo(100);
